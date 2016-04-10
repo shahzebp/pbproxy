54c8a68c430163a79789a7fc3e2e8f150e2d0d67
shahzeb <shahzeb@localhost.localdomain>
2016-04-10 03:16:44 -0400

    Encryption Decryption working

--- pbproxy.c
+++ pbproxy.c
@@ -10,16 +10,18 @@
 #include <fcntl.h>
 #include <string.h>
 
-#include <openssl/aes.h>
-#include <openssl/hmac.h>
-#include <openssl/buffer.h>
-#include <openssl/rand.h>
+#include <pbproxy.h>
 
-struct proxy_data {
-	char *key;
-	struct sockaddr_in rep_add;
-	int new_sock;
-};
+int init_ctr(struct ctr_state *state, const unsigned char iv[8]) {
+
+	state->num = 0;
+
+	memset(state->ecount, 0, AES_BLOCK_SIZE);
+
+	memset(state->ivec + 8, 0, 8);
+
+	memcpy(state->ivec, iv, 8);
+}
 
 char * read_key(const char *filename) {
 
@@ -51,11 +53,104 @@ char * read_key(const char *filename) {
 }
 
 void * server_routine(void * ptr) {
+
+	pthread_detach(pthread_self());
+
+	int accepted_session_fd = -1, n = 0;
+	char buffer[4096];
+	int flags;
+	bool ssh_done = false;
+
 	printf("New server proxy thread\n");
-	while (1)
-		;
+
+	struct proxy_data *p_data = (struct proxy_data *)ptr;
+
+	accepted_session_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (connect(accepted_session_fd, (struct sockaddr *)&p_data->rep_add,
+		sizeof(p_data->rep_add)) == -1) {
+		printf("Connect failed\n");
+		pthread_exit(NULL);
+	} else
+		printf("Connection Established Successfully\n");
+
+	if (-1 == (flags = fcntl(p_data->new_sock, F_GETFL)))
+		pthread_exit(NULL);
+
+	fcntl(p_data->new_sock, F_SETFL, flags | O_NONBLOCK);
+
+	if (-1 == (flags = fcntl(accepted_session_fd, F_GETFL)))
+		pthread_exit(NULL);
+
+	fcntl(accepted_session_fd, F_SETFL, flags | O_NONBLOCK);
+
+	memset(buffer, 0, sizeof(buffer));
+
+	struct ctr_state state;
+	AES_KEY aes_key;
+	unsigned char iv[8];
+	
+	if (AES_set_encrypt_key(p_data->key, 128, &aes_key) < 0) {
+		fprintf(stderr, "Set encryption key error!\n");
+		exit(1);
+	}
+	
+	while (1) {
+		while ((n = read(p_data->new_sock, buffer, 4096)) >= 0) {
+			if (n == 0)
+				pthread_exit(NULL);
+
+			memcpy(iv, buffer, 8);
+
+			unsigned char decryption[n-8];
+			init_ctr(&state, iv);
+
+			AES_ctr128_encrypt(buffer+8, decryption, n-8, &aes_key, state.ivec, state.ecount, &state.num);
+
+			write(accepted_session_fd, decryption, n-8);
+
+			if (n < 4096)
+				break;
+		}
+
+		while ((n = read(accepted_session_fd, buffer, 4096)) > 0) {
+			
+			if (n == 0)
+				pthread_exit(NULL);
+
+			if (n > 0) {
+				if(!RAND_bytes(iv, 8)) {
+					fprintf(stderr, "Error generating random bytes.\n");
+					exit(1);
+				}
+				char *tmp = (char*)malloc(n + 8);
+				memcpy(tmp, iv, 8);
+				
+				unsigned char encryption[n];
+				init_ctr(&state, iv);
+				AES_ctr128_encrypt(buffer, encryption, n, &aes_key, state.ivec, state.ecount, &state.num);
+				memcpy(tmp+8, encryption, n);
+				
+				write(p_data->new_sock, tmp, n + 8);
+				
+				free(tmp);
+			}
+			
+			if (ssh_done == false && n == 0)
+				ssh_done = true;
+			
+			if (n < 4096)
+				break;
+			
+		}
+		if (ssh_done)
+			break;
+	}
+
+	pthread_exit(0);
 
 }
+
 void service_proxy(int listen_port, int dest_port,
 		struct hostent * dest_address, char *key) {
 
@@ -111,8 +206,8 @@ void service_proxy(int listen_port, int dest_port,
 }
 
 void service_client(int dest_port, struct hostent * dest_address, char *key) {
-	int client_fd = -1;
-	int n 		  = 0;
+	
+	int client_fd = -1, n = 0;
 
 	client_fd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -136,7 +231,55 @@ void service_client(int dest_port, struct hostent * dest_address, char *key) {
 	fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
 	fcntl(client_fd, F_SETFL, O_NONBLOCK);
 
+	char buffer[4096];
+
+	struct ctr_state state;
+	unsigned char iv[8];
+	AES_KEY aes_key;
+	
+	if (AES_set_encrypt_key(key, 128, &aes_key) < 0) {
+		printf("Set encryption key error!\n");
+		return;
+	}	
+
 	while (true) {
+		while ((n = read(STDIN_FILENO, buffer, 4096)) > 0) {
+
+			if(!RAND_bytes(iv, 8)) {
+					printf("Error generating random bytes.\n");
+					return;
+			}
+
+			char *tmp = (char*) malloc(n + 8);
+			memcpy(tmp, iv, 8);
+			
+			unsigned char encryption[n];
+			init_ctr(&state, iv);
+			AES_ctr128_encrypt(buffer, encryption, n, &aes_key, state.ivec,
+				state.ecount, &state.num);
+			memcpy(tmp + 8, encryption, n);
+			write(client_fd, tmp, n + 8);
+			free(tmp);
+
+			if (n < 4096)
+				break;
+		}
+
+		while ((n = read(client_fd, buffer, 4096)) > 0) {
+
+			memcpy(iv, buffer, 8);
+
+			unsigned char decryption[n-8];
+			init_ctr(&state, iv);
+				
+			AES_ctr128_encrypt(buffer + 8, decryption, n - 8, &aes_key, state.ivec,
+				state.ecount, &state.num);
+				
+			write(STDOUT_FILENO, decryption, n-8);
+
+			if (n < 4096)
+				break;
+		}
 
 	}
 }
@@ -164,7 +307,7 @@ int main (int argc, char *argv[]) {
 				printf("Unknown option\n");
 				break;
 			default:
-				printf("Check options\n");
+				printf("Check options\nExiting\n");
 				return 0;
 		}
 	}
